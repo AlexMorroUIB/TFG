fa2c5a67ce8f9ed3d9c89ea0b0d71a1cdc1186a9
AlexMorroUIB <alexandre-joan.morro1@estudiant.uib.cat>
2024-11-27 17:57:37 +0100

    main app v0.0

---
+++ BellverDefense/server.js
@@ -0,0 +1,16 @@
+require('aframe');
+require('super-hands');
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const PORT = 80;
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+http.createServer(app).listen(PORT);
---
+++ BellverDefense/src/js/main.js
@@ -0,0 +1,50 @@
+// terra
+AFRAME.registerComponent('terra', {
+  schema: {
+    // scale: {type: 'vector', default: '10 4 1'},
+    // rotation: {type: 'vector', default: '-90 0 0'},
+    material: {type: 'asset', default: '/textures/ferro.png'},
+    culor: {type: 'color', default: 'red'},
+    message: {type: 'string'}
+  },
+  init: function () {
+    let data = this.data
+    console.log(data.message);
+    this.geometry = new THREE.PlaneGeometry(data.scale);
+    // Create material.
+    this.material = new THREE.MeshStandardMaterial({color: data.culor, side: THREE.DoubleSide});
+    // Create mesh.
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+});
+
+// vagoneta
+AFRAME.registerComponent('vagoneta', {
+  schema: {
+    width: {type: 'number', default: 5},
+    height: {type: 'number', default: 5},
+    material: {type: 'asset', default: '/textures/ferro.png'},
+    culor: {type: 'color', default: 'red'},
+    message: {type: 'string'}
+  },
+  init: function () {
+    let data = this.data
+    console.log(data.message);
+    this.geometry = new THREE.PlaneGeometry(data.width, data.height);
+    // Create material.
+    this.material = new THREE.MeshStandardMaterial({color: data.culor, side: THREE.DoubleSide});
+    // Create mesh.
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+  }
+});
+
+
+// Follow component
+/*AFRAME.registerComponent('follow', {
+  schema: {
+    target: {type: 'selector'},
+    speed: {type: 'number'}
+  }
+});*/
+
+// use pool for enemies https://aframe.io/docs/1.6.0/components/pool.html
